5c6db8a5877d6e2a8f28daccef47c879b9b48cdc
zhenghao <hao.zheng@opay-inc.com>
2019-08-24 14:45:45 +0800

    升级gradle

--- app/src/main/java/com/zh/xplan/XPlanApplication.java
+++ app/src/main/java/com/zh/xplan/XPlanApplication.java
@@ -13,7 +13,6 @@ import com.bumptech.glide.Glide;
 import com.module.common.BaseLib;
 import com.module.common.log.LogUtil;
 import com.module.common.utils.CrashUtils;
-import com.squareup.leakcanary.LeakCanary;
 import com.tencent.bugly.crashreport.CrashReport;
 import com.tencent.smtt.sdk.QbSdk;
 import com.zh.swipeback.SlideFinishManager;
@@ -64,17 +63,6 @@ public class XPlanApplication extends Application {
             Glide.get(this);
             ScreenUtil.init(this);
         }
-    }
-
-    private void initLeakCanary() {
-        //在其他代码之前初始化LeakCanary
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
     }
 
     /**
