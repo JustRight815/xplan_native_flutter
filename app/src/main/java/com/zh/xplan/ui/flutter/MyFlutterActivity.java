74dec26ea17b73e1f80cf754a05f750a7ac74636
zhenghao <hao.zheng@opay-inc.com>
2019-08-27 10:05:06 +0800

    添加扫一扫，banner优化

--- app/src/main/java/com/zh/xplan/ui/flutter/MyFlutterActivity.java
+++ app/src/main/java/com/zh/xplan/ui/flutter/MyFlutterActivity.java
@@ -1,14 +1,22 @@
 package com.zh.xplan.ui.flutter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import com.zh.xplan.R;
+import com.zh.xplan.ui.zxing.activity.CaptureActivity;
+import org.jetbrains.annotations.NotNull;
 import io.flutter.facade.Flutter;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.view.FlutterView;
 
 public class MyFlutterActivity extends AppCompatActivity {
+    private static final String NATIVE_CHANNEL = "flutter_open_native";
+    private static final String NATIVE_OPEN_CAPTURE = "flutter_open_capture";
+
     private FlutterView flutterView;
 
     @Override
@@ -30,6 +38,23 @@ public class MyFlutterActivity extends AppCompatActivity {
             }
         };
         flutterView.addFirstFrameListener(listeners[0]);
+
+        /**
+         * Flutter调用native
+         */
+        new MethodChannel(flutterView, NATIVE_CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+
+                    @Override
+                    public void onMethodCall(@NotNull MethodCall methodCall, @NotNull MethodChannel.Result result) {
+                        if (NATIVE_OPEN_CAPTURE.equals(methodCall.method)) {
+                            //打开扫一扫
+                            startActivity(new Intent(MyFlutterActivity.this, CaptureActivity.class));
+                        } else {
+                            result.notImplemented();
+                        }
+                    }
+                });
     }
 
     @Override
